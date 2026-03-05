af5d808fd1c956ea6e85d73c8864c9daeed191be
Osama Farag <ofarag115@gmail.com>
2026-03-05 14:18:56 -0500

    Update Navbar.jsx
    added useEffect and useState for checking if the user scrolled across the site and adds a background border to the navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/Button";
 import { Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const navLinks = [
@@ -13,12 +13,32 @@ const navLinks = [
 
 export const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    // Handle scroll to add background to navbar
+    useEffect(() => {
+
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        }
+        window.addEventListener("scroll", handleScroll);
+        return () => { window.removeEventListener("scroll", handleScroll)};
+    }, []);
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     };
     return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
+    <header 
+        className={`fixed top-0 left-0 right-0 transition-all duration-500
+        ${isScrolled ? "glass-strong py-3" : "bg-transparent py-5"} 
+        z-50`}
+        >
+        
         <nav className="container mx-auto px-6 flex items-center justify-between">
             <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
                 OF<span className="text-primary">.</span>
