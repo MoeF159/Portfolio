3e201c69a1321227da69250fa7e592d693279c04
Osama Farag <ofarag115@gmail.com>
2026-03-06 14:02:56 -0500

    created a projects variable with placeholder values in Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,23 @@
 
+const projects = [
+    {
+        title: 'Project 1',
+        description: 'Description of project 1',
+        image: '/projects/project1.png',
+        tags: ['React', 'Node.js'],
+        link: '#',
+        github: '#'
+    },
+
+    {
+        title: 'Project 2',
+        description: 'Description of project 2',
+        image: '/projects/project2.png',
+        tags: ['Python', 'Django'],
+        link: '#',
+        github: '#'
+    }
+];
 
 export const Projects = () => {
     return <section></section>;
