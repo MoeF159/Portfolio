8f0df2ef815cc8d721b10e08482a5bbde8f8b757
Osama Farag <ofarag115@gmail.com>
2026-03-07 12:27:10 -0500

    Created testimonials object

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,4 +1,25 @@
+const testimonials = [
+    {
+        quote: "Built a stock market tracker from scratch with watchlists, OTP login, and price alerts—super impressive application that I use frequently!",
+        author: "Mohamed Ahmed",
+        role: "Pilot, Delta Airlines",
+        avatar: "https://randomuser.me/api/portraits/men/14.jpg",
+    },
+    {
+        quote: "Developed a JavaFX app to automate employee hour and overtime calculations. its clean, simple to use, and really helps me save time when im processing hours for paychecks.",
+        author: "Mitten Pattel",
+        role: "Store Manager, Dunkin",        
+        avatar: "https://randomuser.me/api/portraits/men/11.jpg",
 
+    },
+    {
+        quote: "Whenever something went down at our stores, Osama fixed it fast. Super reliable and knows their stuff!",
+        author: "Emma Thompson",
+        role: "Store Manager, Dunkin",
+        avatar: "https://randomuser.me/api/portraits/women/16.jpg",
+}
+
+]
 
 export const Testimonials = () => {
     return <section></section>;
