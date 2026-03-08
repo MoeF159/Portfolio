581ed6e717750d90d40766ac78cce5787a8457d5
Osama Farag <ofarag115@gmail.com>
2026-03-08 15:06:51 -0400

    wrapped the view all projects button inside an <a> tag so that it becomes clickable

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -111,10 +111,12 @@ export const Projects = () => {
             </div>
             {/* View All Button */}
             <div className="text-center mt-12 animate-fade-in animation-delay-500">
-                <AnimatedBorderButton>
-                    View All Projects
-                    <ArrowUpRight className="w-5 h-5"/>
-                </AnimatedBorderButton>
+                <a href="#">
+                    <AnimatedBorderButton>
+                        View All Projects
+                        <ArrowUpRight className="w-5 h-5"/>
+                    </AnimatedBorderButton>
+                </a>
             </div>
         </div>
     </section>
